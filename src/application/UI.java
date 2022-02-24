bd46a505c4a7397c8f92ae61332bdede9dcd329f
William Marques <junior.stuart34@gmail.com>
2022-02-24 16:49:21 -0400

    board spacing adjustments

--- src/application/UI.java
+++ src/application/UI.java
@@ -123,14 +123,15 @@ public class UI {
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
 		
 		System.out.println("Captured pieces");
+		System.out.println();
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
-		System.out.println(ANSI_RESET);
+		System.out.print(ANSI_RESET);
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
-		System.out.println(ANSI_RESET);
+		System.out.print(ANSI_RESET);
 	}
 	
 }
